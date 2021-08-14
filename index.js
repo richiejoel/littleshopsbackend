b9ecb48d824fa51b25b95961309ca757c61d3eaa
Richard Joel Garcia <34136964+richiejoel@users.noreply.github.com>
2021-08-14 18:18:45 -0500

    Fix push

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var http = require('http');
 var server = http.createServer();
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./littleshops-e51d8-firebase-adminsdk-hwsvv-7c1640d7ba.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
  
 function mensaje(petic, resp) {
 	resp.writeHead(200, {'content-type': 'text/plain'});
@@ -8,6 +16,19 @@ function mensaje(petic, resp) {
 }
 server.on('request', mensaje);
  
+
 server.listen(process.env.PORT || 3000, function () {
   	console.log('La Aplicación está funcionando en el puerto 3000');
 });
+
+export async function sendNotificationEventCreation(){
+	try{
+		var payload = {notification: {title: 'FCM Little shops', 
+		body: 'We are notification'},
+		data:{click_action:"FLUTTER_NOTIFICATION_ACTION"}}
+
+		await admin.messaging().sendToTopic('Events', payload);
+	} catch(error){
+		console.log(error);
+	}
+}
