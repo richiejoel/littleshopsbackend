6d2f0455f40d53b1ea171585807fb67f3048adbf
Richard Joel Garcia <34136964+richiejoel@users.noreply.github.com>
2021-08-15 11:33:32 -0500

    Fix export

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-var http = require('http');
+var http = require("http");
 var server = http.createServer();
 
 const admin = require("firebase-admin");
 const serviceAccount = require("./littleshops-e51d8-firebase-adminsdk-hwsvv-7c1640d7ba.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
- 
 function mensaje(petic, resp) {
-	resp.writeHead(200, {'content-type': 'text/plain'});
-	resp.write('Hola Mundo');
-	resp.end();
+  resp.writeHead(200, { "content-type": "text/plain" });
+  resp.write("Hola Mundo");
+  resp.end();
 }
-server.on('request', mensaje);
- 
+server.on("request", mensaje);
 
 server.listen(process.env.PORT || 3000, function () {
-  	console.log('La Aplicación está funcionando en el puerto 3000');
+  console.log("La Aplicación está funcionando en el puerto 3000");
 });
 
-export async function sendNotificationEventCreation(){
-	try{
-		var payload = {notification: {title: 'FCM Little shops', 
-		body: 'We are notification'},
-		data:{click_action:"FLUTTER_NOTIFICATION_ACTION"}}
+async function sendNotificationEventCreation() {
+  try {
+    var payload = {
+      notification: { title: "FCM Little shops", body: "We are notification" },
+      data: { click_action: "FLUTTER_NOTIFICATION_ACTION" },
+    };
 
-		await admin.messaging().sendToTopic('Events', payload);
-	} catch(error){
-		console.log(error);
-	}
+    await admin.messaging().sendToTopic("Events", payload);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+module.exports = { sendNotificationEventCreation };
